58e66921af8c31042a73d88f5c3543d2b3ce1d76
Roger-Chavez <roger.chavez.03@gmail.com>
2023-04-10 13:28:15 -0700

    Moved actions to the backend

--- tage/input/action/DecelAction.java
+++ tage/input/action/DecelAction.java
@@ -1,18 +1,20 @@
-package a3;
+package tage.input.action;
 
 import tage.*;
-import tage.input.action.AbstractInputAction;
 import net.java.games.input.Event;
 import org.joml.*;
 
-public class AccelAction extends AbstractInputAction {
+import a3.MyGame;
+import a3.ProtocolClient;
+
+public class DecelAction extends AbstractInputAction {
 	private MyGame game;
 	private GameObject av;
 	private Vector3f oldPosition, newPosition;
 	private Vector4f fwdDirection;
 	private ProtocolClient protClient;
 
-	public AccelAction(MyGame g, ProtocolClient p)
+	public DecelAction(MyGame g, ProtocolClient p)
 	{
 		game = g;
 		protClient = p;
@@ -26,11 +28,11 @@ public class AccelAction extends AbstractInputAction {
 		// fwdDirection = new Vector4f(0f, 0f, 1f, 1f);
 		// fwdDirection.mul(av.getWorldRotation());
 		// fwdDirection.mul(0.01f);
-		// newPosition = oldPosition.add(fwdDirection.x(), fwdDirection.y(), fwdDirection.z());
+		// newPosition = oldPosition.add(fwdDirection.x(), fwdDirection.y(),
+		// fwdDirection.z());
 		// av.setLocalLocation(newPosition);
 		// protClient.sendMoveMessage(av.getWorldLocation());
 
-		game.accelerate(time);
+		game.decelerate(time);
 	}
 }
-
