f5e194b312274dc54269e16d2307e964b9727a00
Eric Rodriguez <ericrodriguez4@csus.edu>
2023-04-11 13:52:14 -0700

    Added more info and quit command to the server

--- server/NetworkingServer.java
+++ server/NetworkingServer.java
@@ -1,11 +1,14 @@
 package server;
 
 import java.io.IOException;
+import java.util.Scanner;
+
 import tage.networking.IGameConnection.ProtocolType;
 
 public class NetworkingServer {
 	private GameServerUDP thisUDPServer;
 	private GameServerTCP thisTCPServer;
+	private boolean serverIsUdp;
 
 	public NetworkingServer(int serverPort, String protocol)
 	{
@@ -14,13 +17,46 @@ public class NetworkingServer {
 			if (protocol.toUpperCase().compareTo("TCP") == 0)
 			{
 				thisTCPServer = new GameServerTCP(serverPort);
+				serverIsUdp = false;
+				System.out.println("TCP server ready.");
 			} else
 			{
 				thisUDPServer = new GameServerUDP(serverPort);
+				System.out.println("UDP server ready.");
+				serverIsUdp = true;
 			}
 		} catch (IOException e)
 		{
 			e.printStackTrace();
+		}
+
+		Scanner scanner = new Scanner(System.in);
+		while (true)
+		{
+			System.out.print("server> ");
+			String scan = scanner.nextLine();
+
+			if (scan.toLowerCase().contains("quit") || scan.toLowerCase().equals("q"))
+			{
+				System.out.println("Shutting down.");
+				try
+				{
+					if (serverIsUdp)
+					{
+						thisUDPServer.shutdown();
+					}
+					else
+					{
+						thisTCPServer.shutdown();
+					}
+					scanner.close();
+					System.exit(0);
+				} catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+
+			}
 		}
 	}
 
