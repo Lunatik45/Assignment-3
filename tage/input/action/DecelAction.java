9fcead53e2eb57dc2b0a6e3255ea0c4fe18aaf25
Eric Rodriguez <ericrodriguez4@csus.edu>
2023-04-25 00:00:18 -0700

    Tidy up some code

--- tage/input/action/DecelAction.java
+++ tage/input/action/DecelAction.java
@@ -23,16 +23,6 @@ public class DecelAction extends AbstractInputAction {
 	@Override
 	public void performAction(float time, Event e)
 	{
-		// av = game.getAvatar();
-		// oldPosition = av.getWorldLocation();
-		// fwdDirection = new Vector4f(0f, 0f, 1f, 1f);
-		// fwdDirection.mul(av.getWorldRotation());
-		// fwdDirection.mul(0.01f);
-		// newPosition = oldPosition.add(fwdDirection.x(), fwdDirection.y(),
-		// fwdDirection.z());
-		// av.setLocalLocation(newPosition);
-		// protClient.sendMoveMessage(av.getWorldLocation());
-
 		game.decelerate(time);
 	}
 }
